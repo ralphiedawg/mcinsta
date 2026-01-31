142260a8504eb2e24baa688e01f9a3ccfa566b05
ralphiedawg <russellwpage@gmail.com>
2026-01-31 01:16:58 -0500

    init & je vais finir a demain

---
+++ Instance.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import shutil
+import json
+
+class Instance:
+    numInstances = 0
+    def __init__(self, given_path, given_name, game_version):
+        Instance.numInstances += 1 
+        self.version = game_version
+
+        self.index = self.numInstances
+        if os.path.exists(given_path):
+            self.path = given_path
+        else:
+            print('That path does not in fact exist.')
+            sys.exit()
+        self.name = given_name
+
+
+    def totalInstances(self):
+        return self.numInstances
+
+    def getIndex(self):
+        return self.index
+
+    def move(self, newLoc):
+        current = self.path
+        new = newLoc
+
+        newDir = os.path.dirname(new)
+        if not os.path.exists(newDir):
+            os.makedirs(newDir)
+
+        shutil.move(current, new)
+        print(f'Successfully moved {current} to {new}')
+
+    def delete_instance(self, instancePath):
+        sure = input(f"Are you sure you'd like to delete the instance at path {instancePath}? (y/n): ")
+        if sure == "y":
+            shutil.rmtree(instancePath)
+            print(f"Removed the intance at path {instancePath}")
+        else: 
+            print("Operation Cancelled")
+
+    def save_to_json(self):
+        jason = {
+            'name': self.name,
+            'version': self.version,
+            'path': self.path,
+            'index': self.getIndex()
+        }
+        with open ("instances.json","w") as file:
+            json.dump(jason, file, indent = 4)
+
+if __name__ == "__main__":
+    print("Built by a human: RalphieDawg")
+    print("Who's Jason?")
+    print("Does he own the JSTOR??")
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import Instance
+
+one = Instance.Instance('/Users/ralph/Desktop/projects/mcinsta/instance1', 'balls','1.11' )
+two = Instance.Instance('/Users/ralph/Desktop/projects/mcinsta/instance2', 'ha','1.12' )
+
+one.move("/Users/ralph/Desktop/projects/mcinsta/trois")
